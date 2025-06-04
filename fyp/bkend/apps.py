a4d2113ffedc0aa0c1ce475d899206cd6f0c83dd
YusufRaygal <yusufabdirahman1122@gmail.com>
2025-06-04 12:26:46 +0800

    deleted

--- fyp/bkend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BkendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bkend'
--- fyp/fyp/urls.py
+++
@@ -1,9 +0,0 @@
-# filepath: c:\projects\AIU Addmision\fyp\fyp\urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin panel
-    path('', include('bkend.urls')),  # Include URLs from the `bkend` app
-
-]
